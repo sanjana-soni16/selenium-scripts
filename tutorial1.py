284c65a670404418c34fc395d6224bd6dac9aab3
Sanjana Soni <sanjana.soni1612@gmail.com>
2022-05-17 11:56:55 +0530

    Add files via upload

---
+++ tutorial1.py
@@ -0,0 +1,24 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+PATH = "/Users/sanjana/chromedriver"
+
+driver = webdriver.Chrome(service=Service(PATH))
+
+driver.get("https://www.google.com")
+
+driver.title  # => "Google"
+
+driver.implicitly_wait(0.5)
+
+search_box = driver.find_element(By.NAME, "q")
+search_button = driver.find_element(By.NAME, "btnK")
+
+search_box.send_keys("Selenium")
+search_button.click()
+
+driver.find_element(By.NAME, "q").get_attribute("value")  # => "Selenium"
+
+driver.quit()
