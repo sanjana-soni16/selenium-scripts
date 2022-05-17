83dcd1a60ae2c36d3987b4e9520114d7ae2fa2e6
Sanjana Soni <Sanjana.Soni2@ibm.com>
2022-05-17 18:26:29 +0530

    minor changes

--- tutorial1.py
+++ tutorial1.py
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 service = Service(executable_path=ChromeDriverManager().install())
 
-driver = webdriver.Chrome(executable_path=service)
+driver = webdriver.Chrome(service=service)
 
 # PATH = "/Users/sanjana/chromedriver"
 
